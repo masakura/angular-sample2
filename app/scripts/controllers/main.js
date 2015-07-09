dc3c26bc81bccf88b41b7e5c67da1e2ecd1283e9
Tomo Masakura <tomo.masakura@gmail.com>
2015-07-09 16:37:00 +0900

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSample2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSample2App
+ */
+angular.module('angularSample2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
